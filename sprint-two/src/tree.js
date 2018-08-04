2d3f5bc12d1406afba2b59b52ffc619857ff9cee
enigmatikme <khsu0720@gmail.com>
2018-08-03 21:58:58 -0700

    Complete Tree prompt

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,42 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // is contained
+  var isContained = false;
+  // if current node has target
+  if (this.value === target) {
+    //cointained is true
+    isContained = true;
+  } else {
+  // else
+    for (var i = 0; i < this.children.length; i++) {
+
+      // iterate through children
+      // call contains on each child
+      // set the result to contains 
+      isContained = this.children[i].contains(target);
+      // if contained is true
+      if (isContained) {
+      // return is contained
+        return isContained;
+      }
+    }
+  }
+  return isContained;
+  //return is contained
+    
 };
 
 
