eb4a9c8d9ced24adeb85b0983acde2c5d89305d5
enigmatikme <khsu0720@gmail.com>
2018-08-04 17:38:56 -0700

    Completed Hash table prompts

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,12 +7,47 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  // get value at index
+  var bucket = this._storage.get(index);
+  // create bucket
+  //var bucket = [];
+  // create tuple
+  var newTuple = [k, v];
+  // if value is undefined
+  if (bucket === undefined) {
+    // place tuple in bucket
+    bucket = [];
+    bucket.push(newTuple);
+  } else {
+    // if the key value pair is the same
+    var hasTupleAlready = false;
+    _.each(bucket, function(tuple) {
+      if (tuple[0] === k) {
+        hasTupleAlready = true;
+        if (tuple[1] !== v) {
+          tuple[1] = v; 
+        }
+      }
+    });
+    if (!hasTupleAlready) {
+      bucket.push(newTuple);
+    }
+  }
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index); 
+  // get the bucket
+  var currentBucket = this._storage.get(index);
+  // loop through the buket
+  var retrievedItem;
+  _.each(currentBucket, function(item) {
+    if (item[0] === k) {    
+      retrievedItem = item[1];
+    }
+  });
+  return retrievedItem;
 };
 
 HashTable.prototype.remove = function(k) {
